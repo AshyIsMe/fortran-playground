ff630cac8791f00190152702dff060426effb150
Aaron Ash <aaron.ash@gmail.com>
2021-08-30 20:01:46 +1000

    test: iota_1D and iota_2D

--- app/main.f90
+++ app/main.f90
@@ -1,13 +1,16 @@
 program main
-  use fortran_playground, only: say_hello, zeroes, iota
+  use fortran_playground, only: say_hello, zeroes, iota_1D, iota_2D
   implicit none
-  integer :: zs(5), is(5)
+  integer :: zs(5), is(5), is2(3,4)
 
   zs = zeroes(5)
   print *, 'zs:', zs
 
-  is = iota(5)
+  is = iota_1D(5)
   print *, 'is:', is
+
+  is2 = iota_2D(3,4)
+  print *, 'is2:', is2
 
   call say_hello()
 end program main
--- src/fortran-playground.f90
+++ src/fortran-playground.f90
@@ -2,7 +2,7 @@ module fortran_playground
   implicit none
   private
 
-  public :: say_hello, zeroes, iota
+  public :: say_hello, zeroes, iota_1D, iota_2D
 contains
   subroutine say_hello
     print *, "Hello, fortran-playground!"
@@ -22,14 +22,28 @@ contains
   end function zeroes
 
   !TODO make this work like j: i. 2 3 4
-  pure function iota(a) result(is)
+  pure function iota_1D(a) result(is)
     integer, intent(in) :: a
-    integer ::is (a), i
+    integer :: is (a), i
 
     do i=1,a
       is(i) = i-1
     end do
 
-  end function iota
+  end function iota_1D
+
+  pure function iota_2D(a, b) result(is)
+    integer, intent(in) :: a, b
+    integer :: is (a, b), i, j, n
+
+    n = 0
+    do i=1,a
+      do j=1,b
+        is(i, j) = n
+        n = n + 1
+      end do
+    end do
+
+  end function iota_2D
 
 end module fortran_playground
